cd634111234ca917b936699216b89c97f85dee9d
mabaro3009 <mabaro3009@hotmail.com>
2018-04-29 11:35:43 +0200

    guarda comentaris

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+	@comments = Comment.where(post_id: @post , tipus: 'comment').order("votes DESC")
   end
 
   # GET /posts/new
