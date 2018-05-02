d680588d9bd37690bcb1a1927d3a875e9a93967a
mabaro3009 <mabaro3009@hotmail.com>
2018-05-02 17:38:13 +0200

    heroku again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :twitter, 'twitter_api_key', 'twitter_api_secret'
+	provider :twitter, ENV["twitter_api_key"], ENV["twitter_api_secret"]
+	#provider :twitter, 'twitter_api_key', 'twitter_api_secret'
 	#provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end 
