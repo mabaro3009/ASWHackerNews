ca02c456b21a4668414a1ff78f3ba35cceaaf3ed
carlos <carlinhoaa@hotmail.com>
2018-05-04 00:55:54 +0200

    arreglar error submitkkkkkkkkkkkkjhhhhhhh

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,32 +51,24 @@ class PostsController < ApplicationController
 		else
 			@post.tipo = "url"
 	end
-  #if @post.save
-    #redirect_to newest_path
-  #else
-
-  #end
 
 
-    #respond_to do |format|
+    respond_to do |format|
 	  if (!@post.text?) && (!@post.url?)
         #flash[:alert] = "Error creating new post!"
-        render :new
+        #render :new
 
-        #format.html { render :new}
-        #format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :new}
+        format.json { render json: @post.errors, status: :unprocessable_entity }
 	  elsif (@post.text?) && (@post.url?)
-        render :new
-        #format.html { render :new }
-        #format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       elsif @post.save
-        redirect_to newest_path
-        #format.html { redirect_to newest_path, notice: 'Post was successfully created.' }
-        #format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to newest_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
       else
-        render :new
-        #format.html { render :new }
-        #format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
