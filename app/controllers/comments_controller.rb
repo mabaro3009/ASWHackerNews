245e9b44e1e54a0bf899fbd0b6362cbad7c58418
mabaro3009 <mabaro3009@hotmail.com>
2018-05-13 15:05:12 +0200

    heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -96,10 +96,10 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    #respond_to do |format|
-     # format.html { redirect_to @comment.post, notice: 'Comment was successfully destroyed.' }
-      #format.json { head :no_content }
-    #end
+    respond_to do |format|
+      format.html { redirect_to @comment.post, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
   ##API CALLS
 
