652e6e10eb4ebb0fc47f969a1c597cc59229b94a
novoa97 <novoa199@gmail.com>
2018-05-13 16:04:00 +0200

    Karma de comentarios

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -113,8 +113,13 @@ end
   def create
   @upvote = Upvote.new(upvote_params)
 	@upvote.user_id = current_user.id
+  if @upvote.comment_id.exists?
+    @comment = Comment.find(@upvote.comment_id)
+    @user = User.find(@comment.user_id)
+  else
   @post = Post.find(@upvote.post_id)
   @user = User.find(@post.user_id)
+  end
   @user.update_attribute(:karma, @user.karma + 1)
 
     respond_to do |format|
@@ -150,8 +155,13 @@ end
   # DELETE /upvotes/1.json
   def destroy
 	@upvote = Upvote.find(params[:upvote])
+  if @upvote.comment_id.exists?
+    @comment = Comment.find(@upvote.comment_id)
+    @user = User.find(@comment.user_id)
+  else
   @post = Post.find(@upvote.post_id)
   @user = User.find(@post.user_id)
+  end
   @user.update_attribute(:karma, @user.karma - 1)
     @upvote.destroy
     respond_to do |format|
