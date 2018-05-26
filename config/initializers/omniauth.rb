c9dcb0339c3ae160c622c160d8197df35f92ba62
novoa97 <novoa199@gmail.com>
2018-05-26 10:27:23 +0200

    Solucionado Swagger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
 #descomentar para hacer el heroku
-	provider :twitter, ENV["twitter_api_key"], ENV["twitter_api_secret"]
+	#provider :twitter, ENV["twitter_api_key"], ENV["twitter_api_secret"]
 
 #Descomentar para usar el local
-	#provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+	provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
