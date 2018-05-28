359a0d66fd7e1716440d3fb2837b4bf0efd87967
carlos <carlinhoaa@hotmail.com>
2018-05-28 15:29:22 +0200

    gato

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -170,7 +170,7 @@ class CommentsController < ApplicationController
   end
 
   def api_get_comment_from_post
-    @comments = Comment.where(:post_id => params[:post_id])
+    @comments = Comment.where(:post_id => params[:post_id], :tipus => 'comment')
     render json: @comments, status: :ok
 
   end
